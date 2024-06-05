d923c944e99d4992a50761a7fe1f6bafed4f1a3c
Dorerishi <hrushikeshraghvendra@gmail.com>
2024-06-05 18:05:45 +0530

    Os error fixed and filter_maliciuos_keyword added for functionality

--- socmint/root/views.py
+++ socmint/root/views.py
@@ -4,10 +4,37 @@ from django.template import loader
 from django.http import JsonResponse
 from api.reddit_api import fetch_hot_posts, search_posts, fetch_post_details
 from api.predictWrapper import predict,predictBin
-#from api.filter_predict import filter_malicous_keywords
 
 # Create your views here.
-
+def filter_malicous_keywords(posts):
+    malicous_posts={}
+    for post in posts:
+        post_id=posts[post]
+        details=fetch_post_details(post_id)
+        malicous_title=predictBin(details['Title'])
+        malicous_comment=False
+        if malicous_title==True or malicous_comment==True:
+            print("malicous Title detected")
+            malicous_posts[post]=posts[post]
+            break
+        for comment in details["Comments"]:
+            mal_com_temp=predictBin(comment["Comment Body"])
+            if mal_com_temp==True:
+                if comment['Comment Author']=="AutoModerator":
+                    continue
+                elif comment["Comment Body"]=="[removed]":
+                    continue
+                else:
+                    print(comment)
+                    malicous_comment=True
+                    break
+        if malicous_title==True or malicous_comment==True:
+            print("malicous content detected")
+            malicous_posts[post]=posts[post]
+        else:
+            print("no malicous content detected")
+    return malicous_posts
+    pass
 
 def home(request):
     return render(request,'home.html')
@@ -32,8 +59,8 @@ def reddit_api_view(request):
     
     if keyword:
         data['search_results'] = search_posts(keyword)
-        y=predictBin("hello")
-        #data['search_results']=filter_malicous_keywords(data['search_results'])
+        #y=predictBin("hello","./api/Kaviel-threat-text-classifier/")
+        data['search_results']=filter_malicous_keywords(data['search_results'])
     
     if post_id:
         data['post_details'] = fetch_post_details(post_id)
