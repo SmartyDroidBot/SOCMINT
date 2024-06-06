0062742f24f94128d15569a540ae27406ed89197
SmartyDroidBot <eshaanbhatu@gmail.com>
2024-06-06 17:05:58 +0530

    rerouted home

--- socmint/root/urls.py
+++ socmint/root/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/',views.home,name='home'),
+    path('',views.home,name='home'),
     path('twitter/',views.twitter,name='twitter'),
     path('reddit/',views.reddit,name='reddit'),
     path('telegram/',views.telegram,name='telegram'),
